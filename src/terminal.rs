2201757d5c5e6af0768b1b06ff4d20a77354f17b
Mojtaba Amirabadian <mtb.amirabadian@software.bio.nrdc.ir>
2023-09-10 12:01:02 +0330

    Manage cursor position with keys

--- src/terminal.rs
+++ src/terminal.rs
@@ -1,9 +1,16 @@
+use crate::Position;
+use std::io::{self, stdout, Write};
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::{IntoRawMode, RawTerminal}; 
+
 pub struct Size {
     pub width: u16,
     pub height: u16,
 }
 pub struct Terminal {
     size: Size,
+    _stdout: RawTerminal<std::io::Stdout>,
 }
 
 impl Terminal {
@@ -14,9 +21,41 @@ impl Terminal {
                 width: size.0,
                 height: size.1,
             },
+            _stdout: stdout().into_raw_mode()?,
         })
     }
     pub fn size(&self) -> &Size {
         &self.size
+    }
+    pub fn clear_screen() {
+        // print!("\x1b[2J");
+        print!("{}", termion::clear::All);
+    }
+    pub fn cursor_position(position: &Position) {
+        let Position{mut x, mut y} = position;
+        x = x.saturating_add(1);
+        y = y.saturating_add(1);
+        let x = x as u16;
+        let y = y as u16;
+        print!("{}", termion::cursor::Goto(x, y));
+    }
+    pub fn flush() -> Result<(), std::io::Error> {
+        io::stdout().flush()
+    }
+    pub fn read_key() -> Result<Key, std::io::Error> {
+        loop {
+            if let Some(key) = io::stdin().lock().keys().next() {
+                return key;
+            }
+        }
+    }
+    pub fn cursor_hide() {
+        print!("{}", termion::cursor::Hide);
+    }
+    pub fn cursor_show() {
+        print!("{}", termion::cursor::Show);
+    }
+    pub fn clear_current_line() {
+        print!("{}", termion::clear::CurrentLine);
     }
 } 
