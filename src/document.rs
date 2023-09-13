68b5e8cb587aab769902192cb9664a5e9a0e3fcb
Mojtaba Amirabadian <mtb.amirabadian@software.bio.nrdc.ir>
2023-09-13 10:31:11 +0330

    Complete highlighting

--- src/document.rs
+++ src/document.rs
@@ -19,9 +19,7 @@ impl Document {
         let file_type = FileType::from(filename);
         let mut rows = Vec::new();
         for value in contents.lines() {
-            let mut row = Row::from(value);
-            row.highlight(file_type.highlighting_options(), None);
-            rows.push(row);
+            rows.push(Row::from(value));
         }
         Ok(Self {
             rows,
@@ -49,9 +47,9 @@ impl Document {
         }
         #[allow(clippy::indexing_slicing)]
         let current_row = &mut self.rows[at.y];
-        let mut new_row = current_row.split(at.x);
-        current_row.highlight(self.file_type.highlighting_options(), None);
-        new_row.highlight(self.file_type.highlighting_options(), None); 
+        let new_row = current_row.split(at.x);
+        #[allow(clippy::arithmetic_side_effects)]
+        self.rows.insert(at.y + 1, new_row);
     } 
     pub fn insert(&mut self, at: &Position, c: char) {
         if at.y > self.len() {
@@ -60,19 +58,24 @@ impl Document {
         self.dirty = true;
         if c == '\n' {
             self.insert_newline(at);
-            return;
-        } 
-        if at.y == self.len() {
+        } else if at.y == self.rows.len() {
             let mut row = Row::default();
             row.insert(0, c);
-            row.highlight(self.file_type.highlighting_options(), None);
             self.rows.push(row);
         } else {
             let row = &mut self.rows[at.y];
             row.insert(at.x, c);
-            row.highlight(self.file_type.highlighting_options(), None);
         }
+        self.un_highlight_rows(at.y)
     }
+
+    fn un_highlight_rows(&mut self, start: usize) {
+        let start = start.saturating_sub(1);
+        for row in self.rows.iter_mut().skip(start) {
+            row.is_highlighted = false;
+        }
+    } 
+
     #[allow(clippy::arithmetic_side_effects)]
     pub fn delete(&mut self, at: &Position) {
         let len = self.len();
@@ -84,12 +87,11 @@ impl Document {
             let next_row = self.rows.remove(at.y + 1);
             let row = self.rows.get_mut(at.y).unwrap();
             row.append(&next_row);
-            row.highlight(self.file_type.highlighting_options(), None);
         } else {
             let row = self.rows.get_mut(at.y).unwrap();
             row.delete(at.x);
-            row.highlight(self.file_type.highlighting_options(), None);
-        } 
+        }
+        self.un_highlight_rows(at.y);
     }
     pub fn cut_row(&mut self, at: &Position) -> Option<String> {
         if at.y >= self.len() {
@@ -102,11 +104,11 @@ impl Document {
     pub fn save(&mut self) -> Result<(), Error> {
         if let Some(file_name) = &self.file_name {
             let mut file = fs::File::create(file_name)?;
-            for row in &self.rows {
-                file.write_all(row.as_bytes())?;
-                file.write_all(b"\n")?;
-            }
             self.file_type = FileType::from(file_name);
+            for row in &mut self.rows {
+                file.write_all(row.as_bytes())?;
+                file.write_all(b"\n")?; 
+            }
             self.dirty = false;
         }
         Ok(())
@@ -150,9 +152,23 @@ impl Document {
         }
         None
     }
-    pub fn highlight(&mut self, word: Option<&str>) {
-        for row in &mut self.rows {
-            row.highlight(self.file_type.highlighting_options(), word);
+    pub fn highlight(&mut self, word: &Option<String>, until: Option<usize>) {
+        let mut start_with_comment = false;
+        let until = if let Some(until) = until {
+            if until.saturating_add(1) < self.rows.len() {
+                until.saturating_add(1)
+            } else {
+                self.rows.len()
+            }
+        } else {
+            self.rows.len()
+        }; 
+        for row in &mut self.rows[..until] {
+            start_with_comment = row.highlight(
+                &self.file_type.highlighting_options(),
+                word,
+                start_with_comment,
+            ); 
         }
     } 
 }
--- src/editor.rs
+++ src/editor.rs
@@ -47,6 +47,7 @@ pub struct Editor {
     document: Document,
     status_message: StatusMessage,
     quit_times: u8,
+    highlighted_word: Option<String>,
     clipboard: Clipboard,
 }
 
@@ -88,6 +89,7 @@ impl Editor {
             document,
             status_message: StatusMessage::from(initial_status),
             quit_times: QUIT_TIMES,
+            highlighted_word: None,
             clipboard: Clipboard::new().unwrap(),
         }
     }
@@ -134,7 +136,7 @@ impl Editor {
                     } else if moved {
                         editor.move_cursor(Key::Left);
                     }
-                    editor.document.highlight(Some(query));
+                    editor.highlighted_word = Some(query.to_string());
                 }, 
             )
             .unwrap_or(None);
@@ -143,7 +145,7 @@ impl Editor {
             self.cursor_position = old_position;
             self.scroll();
         }
-        self.document.highlight(None);
+        self.highlighted_word = None;
     } 
     fn process_keypress(&mut self) -> Result<(), std::io::Error> {
         let pressed_key = Terminal::read_key()?;
@@ -210,13 +212,21 @@ impl Editor {
             offset.x = x.saturating_sub(width).saturating_add(1);
         }
     } 
-    fn refresh_screen(&self) -> Result<(), std::io::Error> {
+    fn refresh_screen(&mut self) -> Result<(), std::io::Error> {
         Terminal::cursor_hide();
         Terminal::cursor_position(&Position::default());
         if self.should_quit {
             Terminal::clear_screen();
             println!("Goodbye.\r");
         } else {
+            self.document.highlight(
+                &self.highlighted_word,
+                Some(
+                    self.offset
+                        .y
+                        .saturating_add(self.terminal.size().height as usize),
+                ),
+            ); 
             self.draw_rows();
             self.draw_status_bar();
             self.draw_message_bar(); 
